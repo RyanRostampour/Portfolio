4de5d3521525fedb95264f50c81e4d7738d4fa1b
Ryan Rostampour <127817513+Dig1talSky@users.noreply.github.com>
2024-07-26 03:19:40 -0400

    Updated Project links (should be working) and updated favicon

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -12,25 +12,25 @@ function Projects() {
       <div className={styles.projectsContainer}>
         <ProjectCard
           src={mastermind}
-          link="https://github.com"
+          link="https://github.com/Dig1talSky/Mastermind"
           h3="Mastermind"
           p="Numbe Guessing Game"
         />
         <ProjectCard
           src={flightsimulator}
-          link="https://github.com"
+          link="https://github.com/comp2800/project"
           h3="Flight Simulator"
           p="3D Flight Simulator Game"
         />
         <ProjectCard
           src={currencyConverter}
-          link="https://github.com"
+          link="https://github.com/Dig1talSky/CurrencyConverter"
           h3="Currency Converter"
           p="Live Currency Converter"
         />
         <ProjectCard
           src={portfolio}
-          link="https://github.com"
+          link="https://github.com/Dig1talSky/Portfolio"
           h3="Portfolio"
           p="Current Portfolio"
         />
