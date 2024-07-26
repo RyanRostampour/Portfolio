fecd4ba6920f40f92b9e382c14ca078efe05c034
Ryan Rostampour <127817513+Dig1talSky@users.noreply.github.com>
2024-07-26 00:44:24 -0400

    Fixed padding and responsiveness of each section
    Hero Section:
    Fixed padding and responsiveness to browser size.
    
    Projects Section:
    Fixed padding and responsiveness to browser size.
    
    Skills Section:
    Fixed padding and responsiveness to browser size.
    
    Contact Section:
    Fixed padding and responsiveness to browser size.

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -2,7 +2,7 @@ import styles from "./ContactStyles.module.css";
 
 function Contact() {
   return (
-    <section id="contact" className={styles.Container}>
+    <section id="contact" className={styles.container}>
       <h1 className="sectionTitle">Contact</h1>
       <form action="">
         <div className="formGroup">
