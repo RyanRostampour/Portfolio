803e5bdd8e78fa68a34f36a47a21cec8a7b5ecf6
Ryan Rostampour <rostampourr@hotmail.com>
2026-03-05 01:05:39 -0500

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -20,13 +20,6 @@ function App() {
         <div className="orb orb-2" />
         <div className="orb orb-3" />
       </div>
-      <svg className="grain-overlay" aria-hidden="true">
-        <filter id="grain-filter">
-          <feTurbulence type="fractalNoise" baseFrequency="0.75" numOctaves="2" stitchTiles="stitch" />
-          <feColorMatrix type="saturate" values="0" />
-        </filter>
-        <rect width="100%" height="100%" filter="url(#grain-filter)" />
-      </svg>
       <Hero />
       <About />
       <Projects />
