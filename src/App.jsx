546cc4e96c4726b4c376c51bb20ba833d0892d89
Ryan Rostampour <rostampourr@hotmail.com>
2026-03-05 01:02:28 -0500

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
       </div>
       <svg className="grain-overlay" aria-hidden="true">
         <filter id="grain-filter">
-          <feTurbulence type="fractalNoise" baseFrequency="0.75" numOctaves="4" stitchTiles="stitch" />
+          <feTurbulence type="fractalNoise" baseFrequency="0.75" numOctaves="2" stitchTiles="stitch" />
           <feColorMatrix type="saturate" values="0" />
         </filter>
         <rect width="100%" height="100%" filter="url(#grain-filter)" />
